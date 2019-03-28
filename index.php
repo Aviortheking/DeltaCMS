6914ffc3078d6bf22d19b8de454e96289a4fa066
Avior <florian.bouillon@delta-wings.net>
2019-03-29 00:03:09 +0100

    Added failover for server configuration

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Please Enable apache mod_rewrite</title>
+	<link rel="stylesheet" href="index.css">
+</head>
+<body>
+	<!--
+		this file will only be seen if there is a missconfiguration in the server
+	-->
+	<div class="container">
+		<div class="wrapper">
+		<div class="content">
+			<div class="item">
+				<h1>There is an Error</h1>
+				<p>Please have apache mod_rewrite enabled</p>
+				<p style="font-size: 1rem;">AdminPanel by <a href="https://delta-wings.net/">Aviortheking</a></p>
+				<p style="font-size: 0.7rem;">Webpage Source: <a href="https://codepen.io/ompmega/pen/tDKCo">Ompmega</a></p>
+			</div>
+		</div>
+		</div>
+	</div>
+</body>
+</html>
