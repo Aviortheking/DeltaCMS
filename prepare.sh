9b5cf2d691b546aec534dc8e8df1f2e6ec45044e
Florian Bouillon <florian.bouillon@delta-wings.net>
2019-04-26 22:13:43 +0000

    Upgrade of Drone.io (#2)
    Fix [#1](issues/1)

--- prepare.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-apt-get update -yqq
-apt-get install -yqq zip tar git
-docker-php-ext-install json
-pecl install xdebug
-docker-php-ext-enable xdebug
-curl -sS https://getcomposer.org/installer | php
-php composer.phar install
